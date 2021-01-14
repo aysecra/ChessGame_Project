18ce35b6cac8081a1f431221721b70dd52fffcfc
Ayşe Karataş <aysecratas@gmail.com>
2021-01-14 06:58:16 +0300

    Last Commit

--- Chess_App/Assets/Scripts/MainMenu.cs
+++ Chess_App/Assets/Scripts/MainMenu.cs
@@ -7,10 +7,14 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
     public void QuitGame()
     {
         Application.Quit();
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
     }
 }
